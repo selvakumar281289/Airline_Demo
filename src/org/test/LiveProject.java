7377713d171fe1448472d9bb54a63e5b4f904fc5
selvakumar281289 <selvakumaranbalagan281289@gmail.com>
2019-11-08 13:03:01 +0530

    SELVA DONE PROJECT

---
+++ src/org/test/LiveProject.java
@@ -0,0 +1,18 @@
+package org.test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LiveProject {
+	public static void main(String[] args) {
+		Set<Integer> l =new HashSet<Integer>();
+		l.add(101);
+		l.add(102);
+		l.add(102);
+		l.add(103);
+		for (Integer itr:l) 
+			System.out.println(itr);
+
+	}
+
+}
