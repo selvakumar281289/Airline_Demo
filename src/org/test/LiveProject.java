975b65f22662e012e0015f3ac25d41bef706792b
selvakumar281289 <selvakumaranbalagan281289@gmail.com>
2019-11-08 13:13:45 +0530

    KUMAR DONE PROJECT

---
+++ src/org/test/LiveProject.java
@@ -0,0 +1,19 @@
+package org.test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LiveProject {
+	public static void main(String[] args) {
+		Set<Integer> l =new HashSet<Integer>();
+		l.add(101);
+		l.add(102);
+		l.add(102);
+		l.add(103);
+		for (Integer itr:l) 
+			System.out.println(itr);
+		System.out.println("This is your empPhone");
+		System.out.println("Based on EmployeeChild");
+	}
+
+}
